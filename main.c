357902bf0abdf29b5aa036c795fcfa334758839f
marchel99 <kubamarchelski@live.com>
2023-08-18 16:32:44 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main (){
 
 
 
-int myArray[]={1,2,3};
+int myArray[]={3,55,1};
 int min;
 
 min=myArray[0];
@@ -13,8 +13,15 @@ min=myArray[0];
 
 for (int i=0;i<3;i++)
 {
-    printf("%d",myArray[i]);
+printf("sprawdzam..%d,aktualne min to %d\n",myArray[i],min);
+    if (myArray[i]<min) {
+        min=myArray[i]; printf(" zmieniam na.. %d",min);
+        };
+
+
 }
+
+printf("minimum to: %d\n",min);
 
 return 0;
 }
